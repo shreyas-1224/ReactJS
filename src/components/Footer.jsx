b3fb4045182557c9341538d13bcda8a3deff1d1e
shreyas <pshreyas1224@gmail.com>
2023-02-03 13:15:52 +0530

    react notes and git ignore file

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Footer(){
-    var getYear = new Date().getFullYear();
-    return(
-        <footer>
-            <p> copyright by ShreyasLifeWorks ©{getYear}</p>
-        </footer>
-    );
-}
